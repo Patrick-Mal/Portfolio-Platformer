2aad4649532afc23a276252b7b26528709113b0d
Patrick <patrick_maloney1@outlook.com>
2022-03-17 17:17:43 +0000

    Directional attack working

--- Platformer/Assets/Scripts/PlayerAttack.cs
+++ Platformer/Assets/Scripts/PlayerAttack.cs
@@ -7,6 +7,22 @@ public class PlayerAttack : MonoBehaviour
     public float damage;
     public float range;
 
+    Vector3 mousePos;
+
+    private void OnDrawGizmos()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.x = mousePos.x - gameObject.transform.position.x;
+        mousePos.y = mousePos.y - gameObject.transform.position.y;
+
+        
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.LookRotation(new Vector2(mousePos.x, mousePos.y), Vector3.forward);
+        //Gizmos.DrawWireCube(gameObject.transform.position, new Vector2(0.5f, 0.5f));
+        Gizmos.color = Color.magenta;
+        Gizmos.matrix = Matrix4x4.TRS(gameObject.transform.position, rotation, Vector3.one);
+        Gizmos.DrawWireCube(Vector2.zero, new Vector2(0.2f, 0.2f));
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +35,16 @@ public class PlayerAttack : MonoBehaviour
     {
         if (Input.GetButtonDown("Attack"))
         {
-            LayerMask actor = LayerMask.GetMask("Actor");
-            Debug.Log("Attack");
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 relMousePos;
             RaycastHit2D[] attackTargets;
-            attackTargets = Physics2D.BoxCastAll(gameObject.transform.position, new Vector2(range, 0.5f), 0f, new Vector2(1,0), 0f, actor);
+            LayerMask actor = LayerMask.GetMask("Actor");
+
+            relMousePos.x = mousePos.x - transform.position.x;
+            relMousePos.y = mousePos.y - transform.position.y;
+
+            float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+            attackTargets = Physics2D.BoxCastAll(gameObject.transform.position, new Vector2(0.2f, 0.2f), angle, relMousePos, range, actor);
             Debug.Log(attackTargets.Length);
             foreach(RaycastHit2D i in attackTargets)
             {
