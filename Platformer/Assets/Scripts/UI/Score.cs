3290b5057141810bc30d165ffa1eee2d9bb1e8e9
Patrick <patrick_maloney1@outlook.com>
2022-04-06 17:15:05 +0100

    Update score UI + organise scripts

--- Platformer/Assets/Scripts/UI/Score.cs
+++ Platformer/Assets/Scripts/UI/Score.cs
@@ -6,15 +6,18 @@ public class Score : MonoBehaviour
 {
     [HideInInspector] public int score;
 
+    GameObject userInterface;
 
     void Start()
     {
         score = 0;
+        userInterface = GameObject.Find("Score");
     }
 
     public void AddScore(int points)
     {
         score += points;
+        userInterface.GetComponent<UnityEngine.UI.Text>().text = score.ToString();
         Debug.Log(score);
     }
 
